8bc7fa7b995b5ec18cc762d52a61dc110d191795
Blixa Morgan <blixa@projectmakeit.com>
2018-01-16 14:55:16 -0600

    use environment variables for obs access

--- init.js
+++ init.js
@@ -4,8 +4,9 @@ const Keys = require('./keys')
 const loadPages = require('./lib/loadPage.js')
 const pages = require('./pages.json')
 const OBSWS = require('./lib/obs.js')
-const obs = new OBSWS('localhost','4444','password');
 
+const obs = new OBSWS(process.env.OBS_HOST,process.env.OBS_PORT,process.env.OBS_PASS);
 const myDeck = new Deck
+
 myDeck.pages = loadPages(pages,{deck:myDeck,obs:obs})
 setTimeout(function(){myDeck.draw()},100);
